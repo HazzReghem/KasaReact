839456efd050a5a0d91bbfbf239ee13de52f2a00
Thibault Roussel <thibault.roussel@gmx.com>
2023-03-08 09:51:00 +0100

    nettoyage du code

--- kasa/src/utils/GlobalStyle.jsx
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components'
-
-const StyledGlobalStyle = createGlobalStyle`
-* {
-    font-family: 'Montserrat', 'Trebuchet MS', Helvetica, sans-serif;
-  }
-  `
-
-export default StyledGlobalStyle
--- kasa/src/utils/colors.js
+++
@@ -1,7 +0,0 @@
-const colors = {
-primary : '#FF6060',
-white: '#FFFFFF',
-black: '#000000',
-}
-
-export default colors
