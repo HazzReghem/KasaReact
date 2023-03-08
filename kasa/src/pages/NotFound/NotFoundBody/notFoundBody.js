82a7e2be964c9ab4150bfdf0cded17c28b76dc74
Thibault Roussel <thibault.roussel@gmx.com>
2023-03-08 10:02:21 +0100

    corrections/Footer responsive

--- kasa/src/pages/NotFound/NotFoundBody/notFoundBody.js
+++ kasa/src/pages/NotFound/NotFoundBody/notFoundBody.js
@@ -6,7 +6,7 @@ const NotFoundBody = () => {
     return (
         <div className='error_container__body'>
             <p className="error_container__body__message_number">404</p>
-            <p className="error_container__body__message_oups">Oups! la page que<br /> vous demandez n'existe pas.</p>
+            <p className="error_container__body__message_oups">Oups ! la page que<br /> vous demandez n'existe pas.</p>
             <p className="error_container__body__message_back"><Link to="/home" >Retourner sur la page d'accueil</Link></p>
 
         </div>
